aa836ba6af6e3ab63f5cfc055e6edaecb3c15161
Luana Costa <luanagabrielescosta@gmail.com>
2019-06-13 10:38:54 -0300

    Tentando umas paradas

---
+++ luanaFunctions.py
@@ -0,0 +1,6 @@
+# c) Exiba os caminhos percorridos por todos os robôs que entraram no terreno de busca, ordenados crescentemente pela distância total percorrida
+
+'''
+[(id, instanteAreaAfetada, ponto, vitimasNaVisibilidade)...]
+'''
+def caminhosPercorridos(lista):
